d2863328a8bed035581bc251a22e8f9007a03ab9
MalkiNcz <jakubmalek07@gmail.com>
2024-10-07 20:31:17 +0200

    change buttons styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
   const router = useRouter();
   return (
 	<main className="flex justify-center items-center h-screen flex-col"> 
-    <h1 className="font-bold text-[4vw]">Cock Down Shooter</h1>
-    <button onClick={() => {router.push("/play")}} className="border-[0.2vw] rounded-[0.5vw] w-[12vw] h-[5vw] text-[3vw] hover:bg-green-800">Play</button>
-    <button className="border-[0.2vw] rounded-[0.5vw] w-[10vw] h-[4vw] text-[2vw] mt-[1vw] hover:bg-green-700">Settings</button>
+    <h1 className="font-bold text-[5vw] text-red-600">Cock Down Shooter</h1>   
+    <button onClick={() => {router.push("/play")}} className="border-[0.4vw] rounded-[0.6vw] w-[15vw] h-[6vw] text-[3vw] hover:bg-green-800 font-bold">Play</button>
+    <button className="border-[0.4vw] rounded-[0.6vw] w-[12.5vw] h-[4vw] text-[2vw] mt-[1vw] hover:bg-green-700 font-bold">Settings</button>
     
     
 	</main>
