153eb4c0dca6a34a6c796a843724a2bb369e30ab
KoblizekXD <janprokupek@centrum.cz>
2024-10-08 20:08:14 +0200

    Add randomized spawning of objects

---
+++ src/app/play/layout.tsx
@@ -0,0 +1,18 @@
+import { Metadata, Viewport } from "next";
+import Play from "./page";
+
+export const metadata: Metadata = {
+  title: "Cock Down Shooter 🐓",
+  description: "Play Cock Down Shooter today!",
+};
+
+export const viewport: Viewport = {
+  maximumScale: 1,
+  initialScale: 1,
+  width: 'device-width',
+  userScalable: false
+}
+
+export default function PlayLayout() {
+  return <Play />
+}
--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,34 +1,74 @@
 "use client"
 
-import Image from 'next/image'
-import { useEffect, useState } from 'react';
+import { useSpring, animated } from '@react-spring/web';
+import Image from 'next/image';
+import React, { useEffect, useRef, useState } from 'react';
+
+const FlyingChicken = ({begin, end}: {begin: number, end: number}) => {
+
+  const [screenWidth, setScreenWidth] = useState(0);
+
+  useEffect(() => {
+    setScreenWidth(window.innerWidth + 1);
+  }, []);
+
+  const springs = useSpring({
+    from: { left: 0, top: begin },
+    to: { left: screenWidth, top: end },
+    config: { duration: 2000 },
+  })
+
+  return (
+    <animated.div 
+      style={springs}
+      className={`absolute w-24 h-24 cursor-crosshair border`}>
+    </animated.div>
+  );
+}
 
 export default function Play() {
-
   const [clicked, setClicked] = useState(false);
+  const [chickens, setChickens] = useState<React.ReactNode[]>([]);
+  const mainRef = useRef<HTMLElement>(null);
 
   useEffect(() => {
     if (clicked) {
       setTimeout(() => {
-        console.log('timer gone');
-        
         setClicked(false);
       }, 200);
     }
   }, [clicked]);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // appendChick();
+    }, 500);
+
+    return () => clearInterval(interval);
+  })
+
+  const appendChick = () => {    
+    setChickens([...chickens, <FlyingChicken begin={randomInt(window.innerHeight)} 
+      end={randomInt(window.innerHeight)} />])
+  }
+
   return (
-	<main> 
-    
+	<main ref={mainRef}>
     <Image className={`cursor-pointer ${clicked ? 'scale-110' : 'hover:scale-105'} w-[5vw] h-[5vw] transition-transform`}
-        src="/menu-btn.png"
-        onClick={(e) => {
-          setClicked(true);
-        }}
-        width={"256"}
-        height={"256"}
-        alt="ERR:FNF"
-      />
+      src="/menu-btn.png"
+      onClick={(e) => {
+        setClicked(true);
+        appendChick();
+      }}
+      width={"512"}
+      height={"512"}
+      alt="ERR:FNF"
+    />
+    {...chickens}
 	</main>
   );
+}
+
+function randomInt(max: number) {
+  return Math.floor(Math.random() * max);
 }
