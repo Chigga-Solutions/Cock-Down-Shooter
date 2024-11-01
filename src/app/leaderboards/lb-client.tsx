60777a8a5dd1854d2da071f372f5a4b312b9cb88
KoblizekXD <janprokupek@centrum.cz>
2024-11-01 09:30:59 +0100

    Add initial leaderboard page

---
+++ src/app/leaderboards/lb-client.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import { luckiestGuy } from "@/components/settings-menu";
+import '@/app/globals.css';
+import { LogOut } from "lucide-react";
+import { ReactNode } from "react";
+import { useRouter } from "next/navigation";
+
+export interface LeaderboardEntry {
+  name: string;
+  score: number;
+}
+
+const EntryWrapped = ({ lbEntry, position }: { lbEntry: LeaderboardEntry, position: number }) => {
+
+  const bgStyle = position === 0 ? 'bg-gradient-to-b from-yellow-400 to-yellow-500' 
+    : position === 1 ? 'bg-gradient-to-b from-gray-400 to-gray-500' 
+    : position === 2 ? 'bg-[#CD7F32]' 
+    : 'bg-gradient-to-b from-blue-500 to-blue-600';
+  const borderStyle = position === 0 ? 'border-yellow-400'
+    : position === 1 ? 'border-gray-400'
+    : position === 2 ? 'border-[#CD7F32]'
+    : 'border-blue-500';
+
+  return (
+    <tr className="hover:scale-105 cursor-pointer ease-in transition-transform" key={position}>
+      <td className={`py-2 ${bgStyle} ${borderStyle} rounded-l border-y border-l`}>{position + 1}.</td>
+      <td className={`border-y ${borderStyle} ${bgStyle}`}>{lbEntry.name}</td>
+      <td className={`border-y ${borderStyle} ${bgStyle} rounded-r border-r`}>{lbEntry.score}</td>
+    </tr>
+  )
+}
+
+export function LbClient() {
+  const router = useRouter();
+  const entries: LeaderboardEntry[] = [{name: 'Negus', score: 69}, {name: 'Negus', score: 69}, {name: 'Negus', score: 69}, {name: 'Negus', score: 69}];
+
+  return (
+    <main className={`h-screen ${luckiestGuy} flex justify-center items-center w-screen`}>
+      <div className='w-[50vw] h-[60vh] gap-y-4 flex flex-col rounded border-[#7F5B3B] border-2 from-[#BF945A] to-[#B48145] bg-gradient-to-b'>
+        <h1 className={`absolute left-1/2 -translate-y-3 text-shadow font-bold text-4xl -translate-x-1/2`}>Leaderboards</h1>
+        <button onClick={() => router.push('/')} title="Back to home" className={'border-2 w-fit rounded hover:scale-110 hover:-rotate-3 ml-3 mt-3 p-0.5 from-blue-500 to-blue-600 bg-gradient-to-b transition-transform'}>
+          <LogOut size={32} strokeWidth={3} />
+        </button>
+        <div className="flex-1 justify-center items-center flex">
+          {entries.length > 0 ? (
+            <table style={{ borderSpacing: "0 10px" }} className="w-full border-separate border-spacing-y-2 *:text-center self-start mx-2 max-h-full text-shadow table-auto">
+              <thead>
+                <tr>
+                  <th>Rank</th>
+                  <th>Name</th>
+                  <th>Score</th>
+                </tr>
+              </thead>
+              <tbody className={'gap-y-2'}>
+                {entries.map((entry, i) => (
+                  <EntryWrapped key={i} lbEntry={entry} position={i} />
+                ))}
+              </tbody>
+            </table>
+          ) : <p>No scores found...</p>}
+        </div>
+      </div>
+    </main>
+  )
+}
---
+++ src/app/leaderboards/page.tsx
@@ -0,0 +1,5 @@
+import { LbClient } from "./lb-client";
+
+export default function LeaderboardPage() {
+  return <LbClient />
+}
